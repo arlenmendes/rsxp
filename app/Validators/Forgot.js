9502ffb5af38e3f8e63b500ba5588644b2dd30ad
Arlen Mendes <arlenmsilva@gmail.com>
2019-11-04 16:43:16 -0300

    Change validators to get messages in file.

--- app/Validators/Forgot.js
+++ app/Validators/Forgot.js
@@ -1,8 +1,14 @@
+const Antl = use('Antl')
+
 class Forgot {
   get rules() {
     return {
       email: 'required|email',
     }
+  }
+
+  get messages() {
+    return Antl.list('validation')
   }
 }
 
--- app/Validators/Reset.js
+++ app/Validators/Reset.js
@@ -1,9 +1,15 @@
+const Antl = use('Antl')
+
 class Reset {
   get rules() {
     return {
       token: 'required',
       password: 'required|confirmed',
     }
+  }
+
+  get messages() {
+    return Antl.list('validation')
   }
 }
 
